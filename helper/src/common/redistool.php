42728caa3b1b39c5fd45de3dcf64febf750950e6
Ray <wangxiaolei@songchechina.com>
2018-07-18 15:42:21 +0800

    测试composer组件

---
+++ helper/src/common/redistool.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ray
+ * Date: 2018/7/18
+ * Time: 15:38
+ */
+namespace songchechina;
+
+class RedisTool
+{
+
+    private static $redis = NULL;
+
+    public static function instance($config = 'cache'){
+        if ( ! static::$redis){
+            static::$redis = \Redis_Db::forge($config);
+        }
+    }
+
+    public static function get($key, $default_value = false){
+        static::instance();
+
+        $value = static::$redis->get($key);
+
+        return $value ? $value : $default_value;
+    }
+
+    public static function set($key, $value, $expiration = false){
+        static::instance();
+
+        if ($expiration){
+            $result = static::$redis->set($key, $value, 'EX', $expiration);
+        } else {
+            $result = static::$redis->set($key, $value);
+        }
+
+        return $result;
+    }
+}
